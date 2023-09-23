54153079680c084ec90a9fe315823328adf49bc2
lankeycode <michael.moussaoui@gmail.com>
2023-09-23 23:25:37 +0200

    create database connection

---
+++ Backend/database/db.js
@@ -0,0 +1,20 @@
+import { Sequelize } from "sequelize";
+import dotenv from "dotenv";
+dotenv.config();
+
+export const db = new Sequelize(
+	process.env.DB_NAME,
+	process.env.DB_USER,
+	process.env.DB_PASSWORD,
+	{
+		host: process.env.DB_HOST,
+		dialect: "mysql",
+	}
+);
+
+try {
+	await db.authenticate();
+	console.log("Connection has been established successfully.");
+} catch (error) {
+	console.error("Unable to connect to the database:", error);
+}
--- Backend/index.js
+++ Backend/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import "./database/db.js";
 
 dotenv.config();
 
@@ -8,6 +9,7 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extend: true }));
+
 app.use(
 	cors({
 		origin: ["http://localhost:5173"],
